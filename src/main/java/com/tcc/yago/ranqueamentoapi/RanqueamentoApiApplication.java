9b4dff9b7585beb0f9b0627cecd8b07e94525067
dev.yago <yagoromualdo.yr@gmail.com>
2024-04-18 21:31:30 -0300

    Primeiro commit
    Primeiro commit

---
+++ src/main/java/com/tcc/yago/ranqueamentoapi/RanqueamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.yago.ranqueamentoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanqueamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanqueamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/yago/ranqueamentoapi/RanqueamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.yago.ranqueamentoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanqueamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
